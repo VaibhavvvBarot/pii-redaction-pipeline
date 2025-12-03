6157e4d7af68605320e7b289e8336a708b8e97da
Vaibhav Barot <vaibhavvvbarot@gmail.com>
2025-12-03 08:23:45 -0700

    start lexicon with days and months

---
+++ src/lexicon.py
@@ -0,0 +1,11 @@
+"""PII lexicon - days, months, colors, cities, states."""
+
+DAYS = [
+    "monday", "tuesday", "wednesday", "thursday", 
+    "friday", "saturday", "sunday"
+]
+
+MONTHS = [
+    "january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december"
+]
